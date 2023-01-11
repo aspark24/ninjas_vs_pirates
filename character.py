dd038b80bf16a78a4124dab7a99f16c24b91fcaf
Andrew Park <andrewpark41299@gmail.com>
2023-01-11 14:36:09 -0800

    commit

--- character.py
+++ character.py
@@ -1,34 +1,68 @@
 class Character:
-    def __init__(self)
 
-class Ninja:
-
-    def __init__( self , name ):
+    def __init__(self, name, strength, speed, health):
         self.name = name
-        self.strength = 10
-        self.speed = 5
-        self.health = 100
-    
+        self.strength = strength
+        self.speed = speed
+        self.health = health
+
+    # def health(self, amount):
+    #     if self.health >=amount:
+    #         self.health -=amount
+    #     else:
+    #         print("Player is dead!")
+    #         self.health -=0
+
+
+
+
+class Ninja(Character):
+
+    def __init__(self, name, strength, speed, health):
+        super().__init__(name, strength, speed, health)
+
     def show_stats( self ):
         print(f"Name: {self.name}\nStrength: {self.strength}\nSpeed: {self.speed}\nHealth: {self.health}\n")
 
-    def attack( self , pirate ):
+    def attack( self, pirate, amount):
+        if self.health >=amount:
+            self.health-=amount
+        else:
+                print("ded")
+                self.health -=0
+
         pirate.health -= self.strength
         return self
 
 
-class Pirate:
 
-    def __init__( self , name ):
-        self.name = name
-        self.strength = 15
-        self.speed = 3
-        self.health = 100
+class Pirate(Character):
 
-    def show_stats( self ):
+    def __init__(self, name, strength, speed, health):
+        super().__init__(name, strength, speed, health)
+
+    def show_stats(self):
         print(f"Name: {self.name}\nStrength: {self.strength}\nSpeed: {self.speed}\nHealth: {self.health}\n")
 
-    def attack ( self , ninja ):
+    def attack (self, ninja):
         ninja.health -= self.strength
         return self
 
+n1 = Ninja("Shadow", 10, 5, 100)
+
+p1 = Pirate("Patches", 15, 3, 100)
+
+p1.attack(n1)
+n1.show_stats()
+p1.attack(n1)
+n1.show_stats()
+p1.attack(n1)
+n1.show_stats()
+p1.attack(n1)
+n1.show_stats()
+p1.attack(n1)
+n1.show_stats()
+p1.attack(n1)
+n1.show_stats()
+p1.attack(n1)
+n1.show_stats()
--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from classes.ninja import Ninja
 from classes.pirate import Pirate
+# from classes.character import Character
 
 michelangelo = Ninja("Michelanglo")
 
